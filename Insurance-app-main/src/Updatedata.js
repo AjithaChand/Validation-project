c1c22b29503cb841c553e7462686c40e9c74ae73
AjithaChand <ajithachand994078@gmail.com>
2025-03-29 13:25:02 +0530

    Saved local changes before pulling

--- Insurance-app-main/src/Updatedata.js
+++ Insurance-app-main/src/Updatedata.js
@@ -40,8 +40,8 @@ const Updatedata = ( {selectid} ) => {
     e.preventDefault();
     axios.put(`http://localhost:8000/edituser/${selectid}`,datas)
     .then(res=>{
-      toast.success(res.data.message)
       navigate('/users')
+      toast.success(res.data.message)
     })
     .catch(err=>toast.error(err.response.data.error))
   }
