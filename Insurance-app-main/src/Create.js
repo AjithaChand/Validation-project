2ca966488f142c237dab093913779e955d1443ae
AjithaChand <ajithachand994078@gmail.com>
2025-03-29 19:15:03 +0530

    Saved local changes before pulling

--- Insurance-app-main/src/Create.js
+++ Insurance-app-main/src/Create.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import User from './User';
 
 const Create = () => {
 
@@ -12,6 +13,8 @@ const Create = () => {
         policy:"",
         file:null
     })
+
+    const [userId,setUserId]= useState(null);
 
     const handleFileChange = (e)=>{
         setValues({...values,file:e.target.files[0]})
@@ -32,10 +35,13 @@ const Create = () => {
         })
         .then(res=>{
             toast.success(res.data.message)
+            setUserId(res.data.userId)
         })
         .catch(err=>toast.error(err.response.data.error))
     }
 
+        console.log(userId);
+        
   return (
     <div >
       <form onSubmit={handleSubmit}>
@@ -57,12 +63,13 @@ const Create = () => {
             <input type='text' className='form-control' style={{backgroundColor:"rgba(255, 255, 255, 0.7)"}} onChange={e=>setValues({...values,policy:e.target.value})} />
         </div>
         <div className='mt-3'>
-          <input type='file' onChange={handleFileChange} className='mt-3'/>
           <input type='file' accept='*/*' onChange={handleFileChange} className='mt-3'/>
         </div>
         <button className='btn user-btn mt-3' style={{backgroundColor:"#333",width:"30%"}}>Submit</button>
       </form>
       <ToastContainer position='top-right' autoClose={3000}/>
+      {/* <User id={userId}/> */}
+      {/* <User id={userId}/> */}
     </div>
   )
 }
