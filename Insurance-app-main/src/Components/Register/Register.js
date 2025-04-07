61b4b2cc70f115f6217cf44fadc16025bf1a8bc3
Nisha-lingam <nishalingam1218@gmail.com>
2025-04-07 10:12:54 +0530

    update

--- Insurance-app-main/src/Components/Register/Register.js
+++ Insurance-app-main/src/Components/Register/Register.js
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { apiurl } from '../../url';
 const Register = () => {
-    const[active,setActive]=useState("signup");
+    const [active, setActive] = useState("signup");
     const navigate = useNavigate()
     const [values, setValues] = useState({
         username: "",
@@ -15,47 +15,52 @@ const Register = () => {
         password: ""
     })
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         // email regex
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if(!emailRegex.test(values.email)){
-           return toast.error("Invalid Email")
+        if (!emailRegex.test(values.email)) {
+            return toast.error("Invalid Email")
         }
 
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%&*])[A-Za-z\d!@#$%&*]{8,}$/;
-       if (!passwordRegex.test(values.password)) {
+        if (!passwordRegex.test(values.password)) {
             return toast.error("Password must be 8 characters includes one number one special character")
         }
-        axios.post(`${apiurl}/register`, values)
-            .then(res => {
-                toast.success(res.data.message)
-                navigate("/")
+        
+        try {
+            const res = await axios.post(`${apiurl}/register`, values)
+            toast.success(res.data.message)
+            await axios.post(`${apiurl}/email_for_register`, {
+                email: values.email,
+                username: values.username
             })
-            .catch(err => toast.error(err.response.data.error))
-
+            navigate("/")
+        } catch (err) {
+            toast.error(err.response.data.error)
         }
+    }
 
     return (
-        <div  className='register-container' style={{height:"100vh",width:"100vw"}}>
+        <div className='register-container' style={{ height: "100vh", width: "100vw" }}>
             <div className='register-form'>
                 <form onSubmit={handleSubmit} className='form-data' style={{ width: "35%" }}>
                     <h3 className='text-center text-white mb-5'>Register Form</h3>
                     <div className='button-container'>
-                        <button className={`btn button-group ${active==="login"?"active":""}`} onClick={()=>{setActive("login");navigate("/")}}>Login</button>
-                        <button className={`btn button-group ${active==="signup"?"active":""}`} onClick={()=>setActive("signup")}>Signup</button>
+                        <button className={`btn button-group ${active === "login" ? "active" : ""}`} onClick={() => { setActive("login"); navigate("/") }}>Login</button>
+                        <button className={`btn button-group ${active === "signup" ? "active" : ""}`} onClick={() => setActive("signup")}>Signup</button>
                     </div>
                     <div className='mt-4 form-group'>
                         <label className='text-white'>Username</label>
-                        <input type='text' className='form-control' style={{backgroundColor:"rgba(255, 255, 255, 0.7)"}} onChange={e => setValues({ ...values, username: e.target.value })} placeholder='Enter your name' required />
+                        <input type='text' className='form-control' style={{ backgroundColor: "rgba(255, 255, 255, 0.7)" }} onChange={e => setValues({ ...values, username: e.target.value })} placeholder='Enter your name' required />
                     </div>
                     <div className='mt-4 form-group'>
                         <label className='text-white'>Email</label>
-                        <input type='email' className='form-control' style={{backgroundColor:"rgba(255, 255, 255, 0.7)"}} onChange={e => setValues({ ...values, email: e.target.value })} placeholder='Enter your email' required />
+                        <input type='email' className='form-control' style={{ backgroundColor: "rgba(255, 255, 255, 0.7)" }} onChange={e => setValues({ ...values, email: e.target.value })} placeholder='Enter your email' required />
                     </div>
                     <div className='mt-4 form-group'>
                         <label className='text-white'>Password</label>
-                        <input type='password' className='form-control' style={{backgroundColor:"rgba(255, 255, 255, 0.7)"}} onChange={e => setValues({ ...values, password: e.target.value })} placeholder='Enter your password' required />
+                        <input type='password' className='form-control' style={{ backgroundColor: "rgba(255, 255, 255, 0.7)" }} onChange={e => setValues({ ...values, password: e.target.value })} placeholder='Enter your password' required />
                     </div>
                     <button className='btn mt-4'>Register</button>
                 </form>
