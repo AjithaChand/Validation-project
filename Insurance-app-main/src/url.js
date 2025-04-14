eb997651b485561459db72be43e8b0dd462670c0
Nisha-lingam <nishalingam1218@gmail.com>
2025-04-14 11:24:13 +0530

    update

--- Insurance-app-main/src/url.js
+++ Insurance-app-main/src/url.js
@@ -1,9 +1,9 @@
 
-// module.exports ={
-//     apiurl:"http://192.168.0.118:7009",
-// }
-
-
 module.exports ={
-    apiurl:"http://localhost:7009",
+    apiurl:"http://192.168.0.106:7009",
 }
+
+
+// module.exports ={
+//     apiurl:"http://localhost:7009",
+// }
