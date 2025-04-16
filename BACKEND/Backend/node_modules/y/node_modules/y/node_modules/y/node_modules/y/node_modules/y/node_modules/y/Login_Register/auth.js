801fd6ef28bca161afb7b7246cd04e42c8a88adb
Rio-ux-lang <outputmaker@gmail.com>
2025-04-16 14:45:21 +0530

    updates

---
+++ BACKEND/Backend/node_modules/y/node_modules/y/node_modules/y/node_modules/y/node_modules/y/node_modules/y/Login_Register/auth.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.SECRET_KEY;
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, secret, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+
+
+const verifyAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+      return res.status(403).json({ message: "Admins only." });
+  }
+  next();
+};
+
+const verifyUser = (req, res, next) => {
+  if (req.user.role !== "user") {
+      return res.status(403).json({ message: "Users only." });
+  }
+  next();
+};
+module.exports = {verifyToken,verifyAdmin,verifyUser};
+
+// module.exports =verifyToken;
+
+
+
+
+
+
+
+
