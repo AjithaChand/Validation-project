86485b264b964e3bc1cd7d0cde5b827767ed1c17
Rio-ux-lang <outputmaker@gmail.com>
2025-04-30 13:24:23 +0530

    updates

--- BACKEND/Backend/User_Pages/user_crm.js
+++ BACKEND/Backend/User_Pages/user_crm.js
@@ -12,18 +12,38 @@ const db = require("../db");
 app.get("/get-single-crm-task",(req,res)=>{
 
     const { email } = req.query;
+    console.log("Reeee Email", email);
+    
+    const selectIdQuery = "SELECT emp_id FROM payslip WHERE emp_email = ?";
 
+    db.query(selectIdQuery,[email],(err, empId)=>{
+
+        if(err){
+            return res.status(400).send({message : "Database Error"})
+        }
+
+        if(empId.length === 0 ){
+            return res.status(404).send({ message : "No User Found In This Email"})
+        }
+
+        const emp_Id = empId[0]?.emp_id;
+        console.log("Epppp IDDD", emp_Id);
+        
    const selectQuery = "SELECT * FROM crm_tasks WHERE emp_id  = ?";
 
-   db.query(selectQuery,(err, results)=>{
+   db.query(selectQuery,[emp_Id],(err, results)=>{
 
     if(err){
         console.log("Error is",err);
         return res.status(400).send({ message : "Database Error"})
     }
 
+    console.log("REeeeeeeeeee", results);
+    
     return res.status(200).send(results)
    })
 })
+})
+
 
 module.exports=app;
