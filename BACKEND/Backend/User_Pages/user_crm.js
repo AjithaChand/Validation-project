54c06d4c011f6dd29208317d5cbe37403171f0f9
Rio-ux-lang <outputmaker@gmail.com>
2025-04-30 15:21:16 +0530

    updates

--- BACKEND/Backend/User_Pages/user_crm.js
+++ BACKEND/Backend/User_Pages/user_crm.js
@@ -45,5 +45,72 @@ app.get("/get-single-crm-task",(req,res)=>{
 })
 })
 
+app.post('/update-in-process',(req,res)=>{
 
+    const {taskid,status} = req.body;
+
+    const dayjs = require('dayjs');
+
+    const updatedAt = dayjs().format('YYYY-MM-DD HH:mm:ss');
+
+    const selectQuery = "SELECT status FROM crm_tasks WHERE id  = ?";
+
+    db.query(selectQuery,[taskid],(err, info)=>{
+
+        if(err){
+            return res.status(400).send({ message : "Database Error" })
+        }
+        
+        const getStatus = info[0];
+
+    if(status === "In Progress"){
+
+        const updateQuery = "UPDATE crm_tasks SET status =  ?, updated_at = ? WHERE id = ?";
+
+        db.query(updateQuery,[status,updatedAt,taskid],(err, info)=>{
+
+            if(err){
+                return res.status(400).send({ message : "Database Error"})
+            }
+
+            return res.send({message : "Let's Start"})
+        })
+    }
+
+    else if (status === "On Hold"){
+
+        if(getStatus === "Completed"){
+            return res.status(404).send({ messgae : "Task Already Completed"})
+        }
+
+        const updateQuery = "UPDATE crm_tasks SET status =  ?, updated_at = ? WHERE id = ?";
+
+        db.query(updateQuery,[status,updatedAt,taskid],(err, info)=>{
+
+            if(err){
+                return res.status(400).send({ message : "Database Error"})
+            }
+
+            return res.send({message : "ON-HOLD Task"})
+        })        
+    }
+    else if (status === "Completed"){
+
+        if(getStatus === "On Hold"){
+            return res.status(400).send({ message : "Task Is ON-HOLD Enable In-Process"})
+        }
+
+        const updateQuery = "UPDATE crm_tasks SET status =  ?, updated_at = ? WHERE id = ?";
+
+        db.query(updateQuery,[status,updatedAt,taskid],(err, info)=>{
+
+            if(err){
+                return res.status(400).send({ message : "Database Error"})
+            }
+
+            return res.send({message : "Task Completed"})
+        })           
+     }
+    })
+})
 module.exports=app;
